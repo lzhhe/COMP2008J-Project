30a881766b05da737235a877cabf258ccdb5af3d
lzhhe <13071108606@163.com>
2023-05-15 22:57:56 +0800

    add a new class for find target (need to complete)

---
+++ Monopoly Deal/src/general/UserTarget.java
@@ -0,0 +1,13 @@
+package general;
+
+import java.util.ArrayList;
+
+import card.Card;
+
+public class UserTarget {//to solve the different target
+    public <T> UserTarget (ArrayList<Player> targetPlayer ,ArrayList<Card> targetLCard){
+
+
+    }
+
+}
