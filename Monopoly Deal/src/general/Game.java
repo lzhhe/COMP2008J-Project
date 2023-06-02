403d2c1c0d721e60659bb13bccf5c86d08a23d9d
1348C52 <tianjialingogogo@163.com>
2023-06-02 22:29:25 +0800

    Junit4 test lib& propertyCardTest

---
+++ Monopoly Deal/src/general/Game.java
@@ -0,0 +1,10 @@
+package general;
+
+public class Game {
+
+	public Object getDeck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Monopoly Deal/src/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author tianj
+ *
+ */
+package test;
